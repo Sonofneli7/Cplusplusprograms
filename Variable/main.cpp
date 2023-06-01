92a2d08c8d5b6e7ee92b09453bbfd1d65e719a47
Sonofneli7 <94796497+Sonofneli7@users.noreply.github.com>
2023-06-01 16:25:47 -0400

    Add files via upload
    incorporating variables

---
+++ Variable/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string characterName = "John";
+    int characterAge;
+    characterAge = 35;
+
+    cout <<"There once was a man named " << characterName << endl;
+    cout <<"He was " << characterAge << " years old" << endl;
+    cout <<"He liked the name Geoge" << endl;
+    cout <<"But didn't like being 70" << endl;
+
+return 0;
+
+
+}
