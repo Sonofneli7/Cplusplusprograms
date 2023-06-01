4ddd3114ffebcd69df271d28142b23eae41222cd
Sonofneli7 <94796497+Sonofneli7@users.noreply.github.com>
2023-06-01 16:49:16 -0400

    Add files via upload
    Different datatypes to utilize

---
+++ DataTypes/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char grade = 'A';
+    string pharse = "Nelson is awesome";
+
+    int age = 50; // whole numbers positive or negative (-50)
+    float number = 2.3;   // decimal numbers, positive or negative
+    //double = 3.145789;  //decimal numbers, holds more than float, positive or negative
+    double gpa = 3.2;
+
+    bool isMale = true;   //boolean is true/false value
+
+    cout << gpa << endl;
+
+
+    return 0;
+}
