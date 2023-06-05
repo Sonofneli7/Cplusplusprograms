db9478b0569074707fbd62a380ea2ce93c72c9c9
Sonofneli7 <94796497+Sonofneli7@users.noreply.github.com>
2023-06-05 09:37:25 -0400

    Add files via upload
    Array, list of elements

---
+++ Arrays/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+
+// arrays can hold a lot of info vs. a variable
+int main() {
+    
+    //int luckyNums[ ] = { 4, 8, 15, 16, 23, 42};      // storing elements in the array
+    //cout << luckyNums[0] << endl;    // accessing #4 in list
+    //cout << luckyNums[2] << endl;        // acessing #15
+
+    //luckyNums[0] = 19;       // changing value of element 0
+    //cout << luckyNums[0] << endl;    // first element in list
+
+    //int luckyNums[20] = { 4, 8, 15, 16, 23, 42};   // telling program want to store 20 elements in array   
+  //  luckyNums[10] = 100; 
+    //cout << luckyNums[10] << endl;      // print out 10th element
+    
+    int luckyNums[20];   // declaring the amount
+    luckyNums[0] = 100;   // can put in element one by one
+    cout << luckyNums[0] << endl; 
+ return 0;
+}
+
