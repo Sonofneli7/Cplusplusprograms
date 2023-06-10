e844337636130cf53b52cfdb1c0bcfbf4181af5e
Sonofneli7 <94796497+Sonofneli7@users.noreply.github.com>
2023-06-10 08:50:49 -0400

    WhileLoops

---
+++ WhileLoops/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+ 
+    int index = 6;
+    do{
+        cout << index << endl;
+        index++;
+    } while(index <= 5);                   //put a condition inside the paranthesis
+
+    return 0;                               // do while loop, execute code then check codition.
+}
+
